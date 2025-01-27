ed096335199875b717b445e1e96c1a29921e8c81
Anumol V S <145448275+anumol018@users.noreply.github.com>
2025-01-27 12:44:29 +0530

    marks_percentage_using_file.py

--- marks_percentage_using_file.py
+++ marks_percentage_using_file.py
@@ -1,17 +1,20 @@
-name=input("Enter the name:")
-mark1=int(input("Enter the mark for PHYSICS:"))
-mark2=int(input("Enter the mark for CHEMISTRY:"))
-mark3=int(input("Enter the mark for MATHEMATICS:"))
-
 def percentage_of_marks(name,mark1,mark2,mark3):
     return ((mark1+mark2+mark3)/3)
-          
-percentage=percentage_of_marks(name,mark1,mark2,mark3)
-print("NAME\tPHYSICS\t  CHEMISTRY\tMATHEMATICS\n")
-print(f"{name}\t {mark1}\t  {mark2}\t\t{mark3}\n")
-print(f"PERCENTAGE:{percentage:.2f}\n")
 
-report_card_content=(f"REPORT CARD \n-------------------\nNAME:{name}\nPHYSICS:{mark1}\nCHEMISTRY:{mark2}\nMATHEMATICS:{mark3}\nPERCENTAGE:{percentage:.2f} \n --------------------")
+for i in range(2):
+    name=input("Enter the name:")
+    mark1=int(input("Enter the mark for PHYSICS:"))
+    mark2=int(input("Enter the mark for CHEMISTRY:"))
+    mark3=int(input("Enter the mark for MATHEMATICS:"))
+          
+    percentage=percentage_of_marks(name,mark1,mark2,mark3)
+    print("NAME\tPHYSICS\t  CHEMISTRY\tMATHEMATICS\n")
+    print(f"{name}\t {mark1}\t  {mark2}\t\t{mark3}\n")
+    print(f"PERCENTAGE:{percentage:.2f}\n")
+
+    report_card_content=(f"REPORT CARD \n-------------------\nNAME:{name}\nPHYSICS:{mark1}\nCHEMISTRY:{mark2}\nMATHEMATICS:{mark3}\nPERCENTAGE:{percentage:.2f} \n--------------------")
     
-with open(f"{name}_report_card.txt","w") as file:
-    file.write(report_card_content)
+    with open(f"{name}_report_card.txt","w") as file:
+        file.write(report_card_content)
+
+
